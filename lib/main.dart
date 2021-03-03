a40c3fcff62b2050a3fd6ae7959c475298166215
JayBernard01 <79108625+JayBernard01@users.noreply.github.com>
2021-03-03 11:29:47 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 //application de base pour mettre sur GitHub
 //testing yep
 //ok le projet est modifiable et fonctionne!
+//sauce brune
 void main() {
   runApp(MyApp());
 }
