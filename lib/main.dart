8cfd611878649e471ea4d861757e439874a25de6
Haywire-d <79108425+Haywire-d@users.noreply.github.com>
2021-03-05 10:41:10 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo TestGithub pls',
+      title: 'Flutter Demo TestGithub pls allo',
       theme: ThemeData(
         // This is the theme of your application.
         //
